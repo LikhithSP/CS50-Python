c942020fd0f84349fa9ae03e020e80fb92cc0247
Likhith SP <splikith04@gmail.com>
2024-09-26 06:28:58 +0530

    Add files via upload

---
+++ week_04/bitcoin.py
@@ -0,0 +1,21 @@
+import sys
+import requests
+if len(sys.argv) == 2:
+    try:
+        value = float(sys.argv[1])
+    except:
+        print("Command-line argument is not a number")
+        sys.exit(1)
+else:
+    print("Missing command-line argument")
+    sys.exit(1)
+try:
+    r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    response = r.json()
+    bitcoin = (response['bpi']['USD']['rate_float'])
+    total_amount = bitcoin * value
+    print(f"${total_amount:,.4f}")
+except requests.RequestException:
+    print("RequestException")
+    sys.exit(1)
+
