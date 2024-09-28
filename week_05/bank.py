396cf545904667051f206df946126d5f5c9c173e
Likhith SP <splikith04@gmail.com>
2024-09-28 16:30:14 +0530

    Add files via upload

---
+++ week_05/bank.py
@@ -0,0 +1,13 @@
+def main():
+    greeting = input("Greeting: ")
+    value_to_print = value(greeting)
+    print(f"${value_to_print}")
+def value(greeting):
+    greeting = greeting.lower().strip()
+    if 'hello' in greeting:
+        return 0
+    elif 'h' == greeting[0]:
+        return 20
+    else:
+        return 100
+    
